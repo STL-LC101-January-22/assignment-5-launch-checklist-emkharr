fd66940d453860aae6df75ce16183ae90d86bcad
Emily Harrison <ekharrison.art@gmail.com>
2022-03-17 18:20:57 -0600

    trying more things

--- script.js
+++ script.js
@@ -1,69 +1,47 @@
 // Write your JavaScript code here!
-// import * as helper  from "./scriptHelper.js";
 
-// const { formSubmission } = require("./scriptHelper");
-
+// const {formSubmission} = require ("./scriptHelper");
 
 window.addEventListener("load", function() {
     
-        alert("Reminder: All fields required.");
-        
-    let form = document.getElementById("launchForm").firstElementChild;
-    // let faultyItems = document.getElementById("faultyItems");
-
-    // form.addEventListener("submit", function(event){
-    //     event.preventDefault
-        
-    //     let formSubmission = formSubmission(document, faultyItems, pilot, copilot, fuelLevel, cargoLevel);
-        
-    //     formSubmission;
+    // let listedPlanets;
+    // // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+ 
+    // let listedPlanetsResponse;
+    // listedPlanetsResponse.then(function (result) {
+    //     listedPlanets = result;
+    //     console.log(listedPlanets);
+    // }).then(function () {
+    //     console.log(listedPlanets);
+    //     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
     // })
+     
+    let form = document.querySelector("form");
+    let list = document.getElementById("faultyItems");
+    let pilot = document.querySelector("input[name=pilotName]").value;
+    let copilot = document.querySelector("input[name=copilotName]").value;
+    let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+    let cargoLevel = document.querySelector("input[name=cargoMass]").value;
 
-    // form.addEventListener("submit", function(event) {
-    //     let submission = formSubmission(window.document, listVar, pilotName.value, copilotName.value, fuelLevelAmnt.value, cargoMass.value);
-    //     if(!submission) {
-    //         event.preventDefault();
-    //     }
-    //     if(submission) {
-    //         event.preventDefault();
-    //     };
-
-
-    // let form = document.querySelector("form")
-    let pilotName = document.querySelector("input[name=pilotName]");
-    let copilotName = document.querySelector("input[name=copilotName]");
-    let fuelLevelAmnt = document.querySelector("input[name=fuelLevel]");
-    let cargoMass = document.querySelector("input[name=cargoMass]");
-    let listVar = document.querySelector("#faultyItems");
-    listVar.style.visibility = "hidden";
-    // let formSubmission = formSubmission(document, listVar, pilotName, copilotName, fuelLevelAmnt, cargoMass);
-    
     form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        formSubmission;
-        
-        // if(formSubmission === false) {
-        //     event.preventDefault();
-        // } else if(formSubmission === true) {
-        //     event.preventDefault();
-        // };
+        // event.preventDefault();
+        // formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+
+        let formSubmission = formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+        // formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+
+        if(!formSubmission) {
+            event.preventDefault();
+        } 
+        if(formSubmission) {
+            event.preventDefault();
+        };
         
     })
 
     });
         
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
    
 // });
 
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,68 +17,72 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-    if (!testInput.value) {
+    if (testInput === "") {
         return "Empty"
-    } else if (isNaN(testInput)) {
+    }; 
+    
+    if (isNaN(testInput)) {
         return "Not a Number"
-    } else if (!isNaN(testInput)) {
+    };
+    
+    if (!isNaN(testInput)) {
         return "Is a Number"
     };
 };
+
+
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
     let launchStatusText = document.getElementById("launchStatus");
     let launchStatusStyle = document.querySelector("#launchStatus");
-
-    let list = document.querySelector("#faultyItems");
+    
+console.log(pilot);
 
     if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
         alert("All fields are required.")
+        return false;
     } else if  (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
-        alert ("Pilot and Copilot names must be letters, Fuel level and Cargo Mass must be numbers.")
+       alert ("Pilot and Copilot names must be letters, Fuel level and Cargo Mass must be numbers.")
+        return false;
     };
 
-
+//add another else if with return false
 
     let pilotStatus = document.getElementById("pilotStatus");
     let copilotStatus = document.getElementById("copilotStatus");
     let fuelStatus = document.getElementById("fuelStatus");
     let cargoStatus = document.getElementById("cargoStatus");
 
-    pilotStatus.innerText = `Pilot ${pilot} Ready.`;
-    copilotStatus.innerText = `Copilot ${copilot} Ready.`;
-   
+    pilotStatus.textContent = `Pilot ${pilot} Ready.`;
+    copilotStatus.textContent = `Copilot ${copilot} Ready.`;
+    list.style.visibility = "visible";
 
 
     if(fuelLevel < 10000 && cargoLevel > 10000) {
-        listVar.style.visibility = "visible";
         launchStatusText.textContent = "Shuttle Not Ready for Launch";
         launchStatusStyle.style.color = "rgb(199, 37, 78)";
         fuelStatus.textContent = `Fuel level too low for launch`;
         cargoStatus.textContent = `Cargo mass too heavy for launch`;
-        return false;
+        // return false;
     } else if(fuelLevel < 10000) {
-        listVar.style.visibility = "visible";
         launchStatusText.textContent = "Shuttle Not Ready for Launch";
         launchStatusStyle.style.color = "rgb(199, 37, 78)";
         fuelStatus.textContent = `Fuel level too low for launch`;
         cargoStatus.textContent = `Cargo mass low enough for launch`;
-        return false;
+        // return false;
     } else if(cargoLevel > 10000) {
         launchStatusText.textContent = "Shuttle Not Ready for Launch";
         launchStatusStyle.style.color = "rgb(199, 37, 78)";
-        listVar.style.visibility = "visible";
         fuelStatus.textContent = `Fuel level high enough for launch`;
         cargoStatus.textContent = `Cargo mass too heavy for launch`;
-        return false;
+        // return false;
     } else {
         launchStatusText.textContent = "Shuttle is Ready for Launch";
         launchStatusStyle.style.color = "rgb(65, 159, 106)";
-        listVar.style.visibility = "visible";
         fuelStatus.textContent = `Fuel level high enough for launch`;
         cargoStatus.textContent = `Cargo mass low enough for launch`;
-        return true;
+        // return true;
     }
 }
 
@@ -90,17 +94,15 @@ async function myFetch() {
             return response.json;
         });
     });
-
     return planetsReturned;
 };
 
 function pickPlanet(planets) {
-   
     for(let i = 0; i < planets.length; i++) {
         let i = Math.floor(Math.random() * planets.length);
         return planets[i];
-    }
-}
+    };
+};
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
